8ac2f60db3fc6661f76d650ecc22cbcabc4c975d
stonycat <zhuqiancg@163.com>
2017-04-22 15:55:37 +0800

    ch10 K-均值聚类

---
+++ ch10/kMeans.py
@@ -0,0 +1,122 @@
+#coding=utf-8
+
+from numpy import *
+
+#load data
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines(): #for each line
+        curLine = line.strip().split('\t')
+        fltLine = list(map(float,curLine)) #这里和书中不同 和上一章一样修改
+        dataMat.append(fltLine)
+    return dataMat
+
+#distance func
+def distEclud(vecA,vecB):
+    return sqrt(sum(power(vecA - vecB, 2)))  # la.norm(vecA-vecB) 向量AB的欧式距离
+
+#init K points randomly
+def randCent(dataSet, k):
+    n = shape(dataSet)[1]
+    centroids = mat(zeros((k,n)))#create centroid mat
+    for j in range(n):#create random cluster centers, within bounds of each dimension
+        minJ = min(dataSet[:,j])
+        rangeJ = float(max(dataSet[:,j]) - minJ)
+        centroids[:,j] = mat(minJ + rangeJ * random.rand(k,1))
+    return centroids
+
+#K-均值算法:
+def kMeans(dataSet,k,distMeas=distEclud,createCent=randCent):
+    #参数：dataset,num of cluster,distance func,initCen
+    m=shape(dataSet)[0]
+    clusterAssment=mat(zeros((m,2)))#store the result matrix,2 cols for index and error
+    centroids=createCent(dataSet,k)
+    clusterChanged=True
+    while clusterChanged:
+        clusterChanged=False
+        for i in range(m):#for every points
+            minDist = inf;minIndex = -1#init
+            for j in range(k):#for every k centers，find the nearest center
+                distJI=distMeas(centroids[j,:],dataSet[i,:])
+                if distJI<minDist:#if distance is shorter than minDist
+                    minDist=distJI;minIndex=j# update distance and index(类别)
+            if clusterAssment[i,0] != minIndex:
+                clusterChanged = True
+                #此处判断数据点所属类别与之前是否相同（是否变化，只要有一个点变化就重设为True，再次迭代）
+            clusterAssment[i,:] = minIndex,minDist**2
+        #print(centroids)
+        # update k center
+        for cent in range(k):
+            ptsInClust=dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]
+            centroids[cent,:]=mean(ptsInClust,axis=0)
+    return centroids,clusterAssment
+
+#二分K-均值聚类
+def biKmeans(dataSet,k,distMeas=distEclud):
+    m=shape(dataSet)[0]
+    clusterAssment=mat(zeros((m,2)))
+    centroid0 = mean(dataSet, axis=0).tolist()[0]
+    centList = [centroid0]  # create a list with one centroid
+    for j in range(m):  # calc initial Error for each point
+        clusterAssment[j, 1] = distMeas(mat(centroid0), dataSet[j, :]) ** 2
+    while (len(centList) < k):
+        lowestSSE = inf #init SSE
+        for i in range(len(centList)):#for every centroid
+            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:, 0].A == i)[0],:]  # get the data points currently in cluster i
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)# k=2,kMeans
+            sseSplit = sum(splitClustAss[:, 1])  # compare the SSE to the currrent minimum
+            sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:, 0].A != i)[0], 1])
+            print("sseSplit, and notSplit: ", sseSplit, sseNotSplit)
+            if (sseSplit + sseNotSplit) < lowestSSE: #judge the error
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        #new cluster and split cluster
+        bestClustAss[nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)  # change 1 to 3,4, or whatever
+        bestClustAss[nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
+        print('the bestCentToSplit is: ', bestCentToSplit)
+        print('the len of bestClustAss is: ', len(bestClustAss))
+        centList[bestCentToSplit] = bestNewCents[0, :].tolist()[0]  # replace a centroid with two best centroids
+        centList.append(bestNewCents[1, :].tolist()[0])
+        clusterAssment[nonzero(clusterAssment[:, 0].A == bestCentToSplit)[0],:] = bestClustAss  # reassign new clusters, and SSE
+    return mat(centList), clusterAssment
+
+
+#practice example
+#distance calc function：结合两个点经纬度（用角度做单位），返回地球表面两点之间距离
+def distSLC(vecA, vecB):#Spherical Law of Cosines
+    a = sin(vecA[0,1]*pi/180) * sin(vecB[0,1]*pi/180)
+    b = cos(vecA[0,1]*pi/180) * cos(vecB[0,1]*pi/180) * cos(pi * (vecB[0,0]-vecA[0,0]) /180)
+    return arccos(a + b)*6371.0 #pi is imported with numpy
+
+#draw function
+import matplotlib
+import matplotlib.pyplot as plt
+def clusterClubs(numClust=5):#参数：希望得到的簇数目
+    datList = []
+    for line in open('places.txt').readlines():#获取地图数据
+        lineArr = line.split('\t')
+        datList.append([float(lineArr[4]), float(lineArr[3])])#逐个获取第四列和第五列的经纬度信息
+    datMat = mat(datList)
+    myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
+    #draw
+    fig = plt.figure()
+    rect=[0.1,0.1,0.8,0.8]#创建矩形
+    #创建不同标记图案
+    scatterMarkers=['s', 'o', '^', '8', 'p', \
+                    'd', 'v', 'h', '>', '<']
+    axprops = dict(xticks=[], yticks=[])
+    ax0=fig.add_axes(rect, label='ax0', **axprops)
+    imgP = plt.imread('Portland.png')#导入地图
+    ax0.imshow(imgP)
+    ax1=fig.add_axes(rect, label='ax1', frameon=False)
+    for i in range(numClust):
+        ptsInCurrCluster = datMat[nonzero(clustAssing[:,0].A==i)[0],:]
+        markerStyle = scatterMarkers[i % len(scatterMarkers)]
+        ax1.scatter(ptsInCurrCluster[:,0].flatten().A[0], ptsInCurrCluster[:,1].flatten().A[0], marker=markerStyle, s=90)
+    ax1.scatter(myCentroids[:,0].flatten().A[0], myCentroids[:,1].flatten().A[0], marker='+', s=300)
+    plt.show()
+
+
